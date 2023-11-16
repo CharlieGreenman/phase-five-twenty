b209a9332e2e7152f0e088bd286668029f5e1d20
Charlie Greenman <charlie@razroo.com>
2023-11-16 12:38:13 -0500

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/phase-five-twenty/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'phase-five-twenty-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'phase-five-twenty';
+}
